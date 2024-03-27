2113398618322d138f30a4f4be343ee8622c8dfc
TonyJiangWJ <tonyjiangwj@gmail.com>
2024-03-27 23:47:17 +0800

    修复饿了么签到,优化京东签到和米游社签到

--- vue_configs/js/components/configuration/MiHoYoConfig.js
+++ vue_configs/js/components/configuration/MiHoYoConfig.js
@@ -30,6 +30,7 @@ const MiHoYoConfig = {
       签到识图配置\
     </van-divider>\
     <tip-block style="margin: 0.5rem">区域输入框左滑可以通过滑块输入数值，也可以通过取色工具获取目标区域信息：<van-button style="margin-left: 0.4rem" plain hairline type="primary" size="mini" @click="openGrayDetector">打开取色工具</van-button></tip-block>
+    <tip-block>默认情况下使用控件查找签到按钮，当查找失败时使用找图方式</tip-block>
     <base64-image-viewer title="校验领取图标按钮" v-model="configs.reward_icon"/>
   </div>`
 }
